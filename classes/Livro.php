525da6f39425581e67f00c7dbe89deaccb627e42
E-Wr3nch <nerdgens404@gmail.com>
2025-05-20 23:59:22 -0300

    lógica para setar a categoria vinda por FK feita

--- classes/Livro.php
+++ classes/Livro.php
@@ -1,10 +1,6 @@
 <?php
-    
-    $testeArray = ["Comédia","Ação","Romance","Ficção-Científica","Aventura","Fantasia"];
-
-        
-
-    class Livro{
+class Livro{
+        public array $testeArray = ["Comédia","Ação","Romance","Ficção-Científica","Aventura","Fantasia"];
         private string $categoriaString;
         function __construct(
             public readonly string $nome,
@@ -12,15 +8,34 @@
             public readonly INT $categoriaNum,
             private INT $quantidade
         ){
-            $this->categoriaString = $this->verificarCategoria($categoriaNum);
+           $this->categoriaString = $this->verificarCategoria($categoriaNum);
         }
         public function verificarCategoria($num):string{
-            $countArray = count($testeArray);
+           $countArray = count($this->testeArray);
+            for($i = 0 ;$i<=$countArray-1;$i++){
+                if($i == $num){
+                    $categoria = $this->testeArray[$i];
+                    break;
+                }else{
+                    $categoria = "Categoria não cadastrada";
+                }
+            }
+            return $categoria;
         }
-
+        public function getCategoria():string{
+            return $this->categoriaString;
+        }
+        
     }
     
+    $t = new Livro(
+        "adsad",
+        "adsaa",
+        2,
+        3,
+    );
 
-
-
+    $test = $t->getCategoria();
+    
+    echo $test;
 ?>
