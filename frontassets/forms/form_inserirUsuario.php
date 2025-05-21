85483fb57968b8afc859d234d763c55b345979b8
E-Wr3nch <91322420+E-Wr3nch@users.noreply.github.com>
2025-05-20 22:00:53 -0300

    Colocado todos os arquivos na branch-erick

---
+++ frontassets/forms/form_inserirUsuario.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Cadastro - Ler Mais</title>
+  <link rel="stylesheet" href="../css/style-cadastroUsuario.css">
+</head>
+<body>
+  <div class="container">
+    <div class="form-container">
+      <img src="../img/logo.jpg" alt="Logo Ler Mais" class="logo">
+      <h1>Ler Mais</h1>
+
+      <p class ="error-message">Erro</p>
+
+      <form action="inserirUsuario_action.php" method="post">
+        <label for="nome">Nome:</label>
+        <input type="text" id="nome_usuario" name="nome_usuario" required>
+
+        <label for="sobrenome">Sobrenome:</label>
+        <input type="text" id="sobrenome_usuario" name="sobrenome_usuario" required>
+
+      <label for="telefone">Contato</label>
+<input type="tel" id="contato_usuario"maxlength="15" name="contato_usuario" placeholder="(99) 99999-9999" required>
+ <script>
+    const telefoneInput = document.getElementById('contato_usuario');
+
+    telefoneInput.addEventListener('input', function(e) {
+      let value = e.target.value.replace(/\D/g, ""); 
+
+      if (value.length > 15) value = value.slice(0, 15); 
+      if (value.length > 0) {
+        value = "(" + value;
+      }
+      if (value.length > 3) {
+        value = value.slice(0, 3) + ") " + value.slice(3);
+      }
+      if (value.length > 10) {
+        value = value.slice(0, 10) + "-" + value.slice(10);
+      }
+
+      e.target.value = value;
+    });
+  </script>
+
+
+
+
+        <label for="email">E-mail:</label>
+        <input type="email" id="email_usuario" name="email_usuario" required>
+
+        <label for="senha">Senha:</label>
+        <input type="password" id="senha_usuario" name="senha_usuario" required>
+
+        <label for="confirmar">Confirmar Senha:</label>
+        <input type="password" id="repetir_senha" name="repetir_senha" required>
+
+        <button type="submit">Cadastrar-se</button>
+      </form>
+
+      <p class="login-link">Já tem uma conta? <a href="#">Faça login</a></p>
+    </div>
+
+    <div class="image-container">
+      <div class="overlay"></div>
+    </div>
+  </div>
+</body>
+</html>
