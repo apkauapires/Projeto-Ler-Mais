c7aecb59c67507cfb99da1233647e11c25bcb879
E-Wr3nch <nerdgens404@gmail.com>
2025-06-03 21:07:43 -0300

    teste for each

--- src/view/inicial.php
+++ src/view/inicial.php
@@ -16,18 +16,26 @@
 <body>
     <?php
         require __DIR__ . "/../view/layout/header.php";
-        foreach ($livros as $livro){
+        
 
     ?>
-        <form  action="LivroAluguel.php" method="post">
+        <form  action="../../controllers/alugarLivro.php" method="post">
       <!--<img src="<?php // echo $livro['img_livro'] ?>"> -->
-            <p> <?php echo $livro['nome_categoria']?></p>
+            <p> </p>
+        <select name="teste" id="teste">
+                <?php foreach ($livros as $livro){ ?>
+            <option value="<?php echo $livro['id_categoria']; ?>">
+                <?php echo $livro['nome_categoria']; ?>
+            </option>
+            <?php } ?>
+        </select>
         <!--<p> <?php //echo $livro['estoque_livro']?></p>-->
        <!-- <p> <?php //echo $livro['nome_livro']?></p>-->
             <button value="<?php echo $livro['id_categoria'];?>">Alugar</button>
         </form>
+        
     <?php
-    }
+    
         require __DIR__ . "/../view/layout/footer.php";
     ?>
 </body>
