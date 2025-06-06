a4b49008568e894e380bd6e1554c5a1d8ca276d8
Mateus Nandi <mateusnandi@gmail.com>
2025-06-06 20:00:03 -0300

    Painel do administrador

---
+++ src/view/painel_adm/painel_adm.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <title>Painel do Administrador</title>
+  <link rel="stylesheet" href="../../components/style-painelAdm.css">
+</head>
+<body>
+  <div class="container">
+    <div class="admin-panel">
+      <h1>Painel do Administrador</h1>
+      <div class="botoes">
+        <a href="cadastro_categoria.html" class="botao">Cadastro de Categoria</a>
+        <a href="cadastro_livro.html" class="botao">Cadastro de Livro</a>
+        <a href="listagem_alugueis.html" class="botao">Listagem de Aluguéis</a>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
