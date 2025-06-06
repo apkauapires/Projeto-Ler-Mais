f89ff3df30fc421e9b0b4174cf53bc18df5a4430
E-Wr3nch <nerdgens404@gmail.com>
2025-06-05 22:00:51 -0300

    botao de adicionar e diminuir qtd de livros da sacola funcional

--- src/controllers/sacolaDeLivros.php
+++ src/controllers/sacolaDeLivros.php
@@ -9,11 +9,11 @@
         $src_img = $_POST['img'] ?? 'default.jpg';
         $id_livro = $_POST['id_livro'] ?? null;
         $titulo = $_POST['titulo'] ?? 'Livro desconhecido';
-
     if ($id_livro !== null) {
     // Verifica se o livro já está na sacola
         if (!isset($_SESSION['sacola'][$id_livro])) {
                 $_SESSION['sacola'][$id_livro] = [
+                    'id_livro' => $id_livro,
                     'src_img' => $src_img,
                     'titulo' => $titulo,
                     'quantidade' => 1
@@ -22,6 +22,21 @@
                 // Aumenta a quantidade se já estiver na sacola
                 $_SESSION['sacola'][$id_livro]['quantidade']++;
             }
+    }
+    if ($_GET['tipo'] === "adicao"){
+        $id_livro = $_GET['id_livro'] ?? null;
+        if ($id_livro !== null && isset($_SESSION['sacola'][$id_livro])) {
+            $_SESSION['sacola'][$id_livro]['quantidade']++;
+        }
+    } elseif ($_GET['tipo'] === "subtracao") {
+        $id_livro = $_GET['id_livro'] ?? null;
+        if ($id_livro !== null && isset($_SESSION['sacola'][$id_livro])) {
+            if ($_SESSION['sacola'][$id_livro]['quantidade'] > 1) {
+                $_SESSION['sacola'][$id_livro]['quantidade']--;
+            } else {
+                unset($_SESSION['sacola'][$id_livro]);
+            }
+        }
     }
 
     // Redireciona de volta ou para a sacola
--- src/view/inicial.php
+++ src/view/inicial.php
@@ -26,9 +26,18 @@
                 <li>
                     <?php echo $alugado['titulo'];?>
                     <?php echo $alugado['src_img'];?>
+                    <button type="button" onclick="location.href='../controllers/sacolaDeLivros.php?tipo=adicao&id_livro=<?php echo $alugado['id_livro']; ?>'">
+                        +
+                    </button>
                     Quantidade: <?= $alugado['quantidade'] ?>;
+                    <button type="button" onclick="location.href='../controllers/sacolaDeLivros.php?tipo=subtracao&id_livro=<?php echo $alugado['id_livro']; ?>'">
+                        -
+                    </button>
                 </li>
             <?php } ?>
+            <button type="button" onclick="location.href='../controllers/alugarLivros.php?id_livro=<?php echo $alugado['id_livro']; ?>'">
+                        alugar
+            </button>
         </ul>
     <?php 
         }
