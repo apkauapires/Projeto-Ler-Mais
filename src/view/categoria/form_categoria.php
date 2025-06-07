642387947a8c64b4f4c66f3a18c234c8e51676df
LuizPatricio <luizpatricio1309@gmail.com>
2025-06-07 17:39:49 -0300

    front-end categoria e livro

--- src/view/categoria/form_categoria.php
+++ src/view/categoria/form_categoria.php
@@ -3,14 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <title>Inserir Categoria</title>
+    <link rel="stylesheet" href="style-cadastroCategoria.css">
 </head>
 <body>
+    
     <h1>Inserir Nova Categoria</h1>
+    <div class="insert-categoria>
     <form action="../../controllers/insertCategoria.php" method="post">
         <label for="nome">Nome da Categoria:</label>
         <input type="text" id="nome_categoria" name="nome_categoria" required>
         <br><br>
         <input type="submit" value="Salvar">
     </form>
+    </div>
 </body>
 </html>
