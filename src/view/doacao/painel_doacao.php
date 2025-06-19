85ee2d7e62f73279f714e71d1ddab48682a70389
E-Wr3nch <nerdgens404@gmail.com>
2025-06-19 14:55:31 -0300

    navegacao

--- src/view/doacao/painel_doacao.php
+++ src/view/doacao/painel_doacao.php
@@ -20,7 +20,7 @@
         <input type="hidden" name="qtd_doacao" value="<?= $_GET['qtd_doacao'] ?>">
         <div class="buttons">
           <button type="submit" class="btn-inserir" formaction="index.php?navegation=8" >Inserir Livro Existente</button>
-          <button type="button" class="btn-cadastrar" href="../livro/form_livro.php">Cadastrar Novo Livro</button>
+          <button type="button" class="btn-cadastrar" href="index.php?navegation=3">Cadastrar Novo Livro</button>
         </div>
       </form>
       
